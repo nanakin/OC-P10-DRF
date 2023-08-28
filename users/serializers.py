86d80d7cfa6d067b13a65876c41286eba20013b1
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-08-28 13:10:40 +0200

    add password to User serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 from datetime import date
+from django.contrib.auth.hashers import make_password
 
 REQUIRED_AGE = 15
 
@@ -8,13 +9,21 @@ REQUIRED_AGE = 15
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "birth_date", "can_be_contacted", "can_data_be_shared"]
+        fields = ["id", "username", "birth_date", "can_be_contacted", "can_data_be_shared", "password"]
+        extra_kwargs = {'password': {'write_only': True}}
 
     def validate_birth_date(self, birth_date):
         if date(year=(birth_date.year + REQUIRED_AGE), month=birth_date.month, day=birth_date.day) \
                 > date.today():
             raise serializers.ValidationError('Too young')
         return birth_date
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class UserListSerializer(serializers.ModelSerializer):
